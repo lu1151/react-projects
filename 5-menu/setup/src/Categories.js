9f291acabfd89b56cd2468ef58cd745283f08bc2
lu1151 <luxi.li@outlook.com>
2020-12-26 00:21:21 -0500

    12/26

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
-  return <div className="btn-container">
-    <button className="filter-btn" onClick={() => filterItems("all")}>
-      all
-  </button>
-    <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-      breakfast
-  </button>
-  </div>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button type="button" className="filter-btn" key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;

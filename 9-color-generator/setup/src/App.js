d833511dcce987a87f2f13e80de5dadb70ac7fd4
lu1151 <luxi.li@outlook.com>
2021-01-23 00:13:36 -0500

    01/23/2021

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#fbcbbe").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,9 +24,15 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e) =>
-            setColor(e.target.value)} placeholder="#f15025" className={`${error ? "error" : null}`} />
-          <button className="btn">submit</button>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)} placeholder="#fbcbbe"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type='submit'>
+            submit
+          </button>
         </form>
 
       </section>

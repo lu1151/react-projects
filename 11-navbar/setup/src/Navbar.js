558634014a4cfd66afba21bfe70099a10617e82a
lu1151 <luxi.li@outlook.com>
2021-01-26 23:52:03 -0500

    01/26/2021

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -6,6 +6,18 @@ import logo from './logo.svg'
 const Navbar = () => {
 
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    }
+    else {
+      linksContainerRef.current.style.height = `0px`
+    }
+  }, [showLinks]);
 
   return <nav>
     <div className="nav-center">
@@ -15,18 +27,18 @@ const Navbar = () => {
           <FaBars />
         </button>
       </div>
-      {showLinks && (
-        <div className="links-container show-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return <li key={id}>
-                <a href={url}>{text}</a>
-              </li>
-            })}
-          </ul>
-        </div>
-      )}
+
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+
       <ul className="social-icons">
         {social.map((socialIcon) => {
           const { id, url, icon } = socialIcon;

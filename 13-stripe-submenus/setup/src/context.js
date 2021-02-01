760ae38b4a22a8efa2b26385d1a9c20a45cd9edc
lu1151 <luxi.li@outlook.com>
2021-01-31 23:42:37 -0500

    01/31/2021

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,41 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSiderbarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSiderbarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSiderbarOpen(false);
+    }
+
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true);
+    }
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return (<AppContext.Provider value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu
+    }}>
+        {children}
+    </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}

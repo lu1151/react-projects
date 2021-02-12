8dc7741901356cfd782dfda26afc41f93dcb212d
lu1151 <luxi.li@outlook.com>
2021-02-11 23:49:07 -0500

    02/11/2021

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -31,6 +31,22 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: "DECREASE", payload: id });
   }
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  }
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   useEffect(() => {
     dispatch({ type: "GET_TOTALS" })
   }, [state.cart])
@@ -45,6 +61,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -41,10 +41,29 @@ const reducer = (state, action) => {
             }
         )
         total = parseFloat(total.toFixed(2));
-
         return { ...state, total, amount };
     }
-    return state;
+    if (action.type === "LOADING") {
+        return { ...state, loading: true };
+    }
+    if (action.type === "DISPLAY_ITEMS") {
+        return { ...state, cart: action.payload, loading: false }
+    }
+    if (action.type === "TOGGLE_AMOUNT") {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload.id) {
+                if (action.payload.type === "in") {
+                    return { ...cartItem, amount: cartItem.amount + 1 }
+                }
+                if (action.payload.type === "de") {
+                    return { ...cartItem, amount: cartItem.amount - 1 }
+                }
+            }
+            return cartItem;
+        }).filter((cartItem) => cartItem.amount !== 0);
+        return { ...state, cart: tempCart }
+    }
+    throw new Error("no matching action type");
 }
 
 export default reducer;

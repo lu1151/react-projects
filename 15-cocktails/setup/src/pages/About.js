9504089bdde0949e75f95ffd0dd403692e84af15
lu1151 <luxi.li@outlook.com>
2021-02-14 00:04:56 -0500

    02/13/2021

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti praesentium dolore nihil voluptas suscipit exercitationem saepe. Cum, impedit. Reiciendis, nisi enim, et libero quibusdam minima, laboriosam totam maxime molestiae consequatur dolorum optio perspiciatis nam aut facilis est repellat eos recusandae!
+      </p>
+    </section>
   )
 }
 

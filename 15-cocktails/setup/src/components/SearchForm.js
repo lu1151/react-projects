035132949a3902b0ec7a7d21ec9268eda3e7a1ab
lu1151 <luxi.li@outlook.com>
2021-02-16 00:29:38 -0500

    02/16/2021

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -4,10 +4,29 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
 
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
   )
 }
 
